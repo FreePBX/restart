5e79b2c20f1f2cd29065b45740f5988d8dfa9d77
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-07-12 22:06:30 +0000

    fixes #4426 move sip_notify.conf to core and use core_conf to add sections

--- functions.inc.php
+++ functions.inc.php
@@ -14,14 +14,23 @@
 //Both of these are used for switch on config.php
 
 function restart_get_config($engine) {
-	global $db;
-	global $ext; 
-	switch($engine) {
-		case "asterisk":
-			//generate dialplan here
-			
-		break;
-	}
+  global $db;
+  global $ext; 
+  global $core_conf;
+  switch($engine) {
+    case "asterisk":
+	    if (isset($core_conf) && is_a($core_conf, "core_conf")) {
+        $core_conf->addSipNotify('polycom-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('sipura-check-cfg',array('Event' => 'resync','Content-Length' => '0'));
+        $core_conf->addSipNotify('grandstream-check-cfg',array('Event' => 'sys-control'));
+        $core_conf->addSipNotify('cisco-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('reboot-snom',array('Event' => 'reboot','Content-Length' => '0'));
+        $core_conf->addSipNotify('aastra-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('aastra-xml',array('Event' => 'aastra-xml','Content-Length' => '0'));
+      }
+
+    break;
+  }
 }
 
 function restart_get_devices($grp) {
--- install.php
+++ install.php
@@ -1,7 +1,28 @@
 <?php
-// Backup old sip_notify.conf file so FreePBX can write a new one
-global $amp_conf;
-$astetc = $amp_conf['ASTETCDIR'];
-rename($astetc."/sip_notify.conf",$astetc."/sip_notify.conf.bak");
 
+if (! function_exists("out")) {
+	function out($text) {
+		echo $text."<br />";
+	}
+}
+
+if (! function_exists("outn")) {
+	function outn($text) {
+		echo $text;
+	}
+}
+global $amp_conf;
+
+$src = dirname(__FILE__).'/etc/sip_notify.conf';
+$dest = $amp_conf['ASTETCDIR'].'/sip_notify.conf';
+
+if (is_link($dest) && readlink($dest) == $src) {
+  outn(_("removing symlink to $dest.."));
+  if (unlink($dest)) {
+    out(_("ok"));
+  } else {
+    out(_("FAILED"));
+    out(_("You may have to remove $dest manually"));
+  }
+}
 ?>
