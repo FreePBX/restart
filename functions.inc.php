6105b99d5ed9c74ff7a75b259bf6285e6fd22063
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2012-04-14 17:49:43 +0000

    Merged revisions 13855-13856,13858-13859,13861,13864,13866,13868,13897,13899-13901,13903,13906,13908,13910,13912,13919,13931,13933-13935,13937,13939,13941,13945,13947-13950,13952-13953,13955-13956,13959,13961-13962,13964-13965,13967 via svnmerge from http://www.freepbx.org/v2/svn/modules/branches/2.10
    ........
      r13855 | p_lindheimer | 2012-03-19 13:17:22 -0700 (Mon, 19 Mar 2012) | 1 line
      
      closes #5664 get app_confbridge to work on Asterisk 10
    ........
      r13864 | p_lindheimer | 2012-03-22 15:33:29 -0700 (Thu, 22 Mar 2012) | 1 line
      
      incorrect logic re #5705
    ........
      r13900 | p_lindheimer | 2012-03-23 14:27:18 -0700 (Fri, 23 Mar 2012) | 1 line
      
      fixes #5710 bad syntax thanks for the fix TSM
    ........
      r13906 | p_lindheimer | 2012-03-23 15:50:21 -0700 (Fri, 23 Mar 2012) | 1 line
      
      fixes #5714 don't write out any Content-Length headers in sip_notify_additional.conf
    ........
      r13908 | mbrevda | 2012-03-26 10:02:04 -0700 (Mon, 26 Mar 2012) | 1 line
      
      closes #5707, #5315 - backup editing issues
    ........
      r13919 | mbrevda | 2012-03-27 01:36:07 -0700 (Tue, 27 Mar 2012) | 1 line
      
       * upstream changes
    ........
      r13934 | p_lindheimer | 2012-03-27 11:01:15 -0700 (Tue, 27 Mar 2012) | 1 line
      
      fixes #5734 properly validate new destinations
    ........
      r13948 | mbrevda | 2012-03-31 16:03:09 -0700 (Sat, 31 Mar 2012) | 1 line
      
      re #5750 - ensure function exists before calling
    ........
      r13949 | p_lindheimer | 2012-04-02 16:09:06 -0700 (Mon, 02 Apr 2012) | 1 line
      
      fixes #5752 typo resulting in pin being requested twice
    ........
      r13956 | mbrevda | 2012-04-10 07:44:57 -0700 (Tue, 10 Apr 2012) | 1 line
      
      upstream bug fixes, restart updates for asterisk 1.8

--- functions.inc.php
+++ functions.inc.php
@@ -21,14 +21,18 @@ function restart_get_config($engine) {
   switch($engine) {
     case "asterisk":
 	    if (isset($core_conf) && is_a($core_conf, "core_conf")) {
-			$core_conf->addSipNotify('polycom-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
-			$core_conf->addSipNotify('sipura-check-cfg',array('Event' => 'resync','Content-Length' => '0'));
-			$core_conf->addSipNotify('grandstream-check-cfg',array('Event' => 'sys-control'));
-			$core_conf->addSipNotify('cisco-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
-			$core_conf->addSipNotify('reboot-snom',array('Event' => 'reboot','Content-Length' => '0'));
-			$core_conf->addSipNotify('aastra-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
-			$core_conf->addSipNotify('aastra-xml',array('Event' => 'aastra-xml','Content-Length' => '0'));
-			$core_conf->addSipNotify('reboot-yealink',array('Event' => 'check-sync\;reboot=true','Content-Length' => '0'));
+                        $core_conf->addSipNotify('polycom-check-cfg',array('Event' => 'check-sync'));
+                        $core_conf->addSipNotify('sipura-check-cfg',array('Event' => 'resync'));
+                        $core_conf->addSipNotify('grandstream-check-cfg',array('Event' => 'sys-control'));
+                        $core_conf->addSipNotify('cisco-check-cfg',array('Event' => 'check-sync'));
+                        $core_conf->addSipNotify('reboot-snom',array('Event' => 'reboot'));
+                        $core_conf->addSipNotify('aastra-check-cfg',array('Event' => 'check-sync'));
+                        $core_conf->addSipNotify('aastra-xml',array('Event' => 'aastra-xml'));
+                        $core_conf->addSipNotify('spa-reboot',array('Event' => 'reboot'));
+                        $core_conf->addSipNotify('linksys-cold-restart',array('Event' => 'reboot_now'));
+                        $core_conf->addSipNotify('linksys-warm-restart',array('Event' => 'restart_now'));
+                        $core_conf->addSipNotify('reboot-yealink',array('Event' => 'check-sync\;reboot=true'));
+
         }
 
     break;
