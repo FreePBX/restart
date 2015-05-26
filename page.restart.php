fcd64ece71acd9aa56c36bac498cc3a14aeffa38
James Finstrom <jfinstrom@sangoma.com>
2015-05-26 15:09:22 -0400

    13 visual updates

--- page.restart.php
+++ page.restart.php
@@ -19,56 +19,81 @@ switch ($action) {
 		}
 		break;
 }
-
-?>
-<p>
-<?php
-	echo "<form name='restart' action='' method='post'>\n";
-	echo "<input type='hidden' name='action' value='restart'>\n";
-	echo "<input type='hidden' name='display' value='${display}'>\n";
-
-	echo "<table><tr><td><div class='content'><h2>"._("Restart Phones")."</h2></td></tr>\n";
-	if($restarted)  {
-		echo "<tr><td><b>"._("Restart requests sent!")."</b><br/><br/></td></tr>";
+if(isset($restarted))  {
+	if($restarted){
+		$info = '<div class="well well-info">'._("Restart requests sent!").'</div>';
+	}else{
+		$info = '<div class="well well-warning">'._("Warning: The restart mechanism behavior is vendor specific.  Some vendors only restart the phone if there is a change to the phone configuration or if an updated firmware is available via tftp/ftp/http"). "</div>";
 	}
-	else  {
-		echo "<tr><td><b>"._("Warning: The restart mechanism behavior is vendor specific.  Some vendors only restart the phone if there is a change to the phone configuration or if an updated firmware is available via tftp/ftp/http"). "</b><br/><br/></td></tr>";
-
-	}
-
+}
+$device_list = core_devices_list();
+$info = isset($info)?$info:'<div class="well well-info">'._("Currently, only Aastra, Snom, Polycom, Grandstream and Cisco devices are supported.").'</div>';
 ?>
-	<tr><td valign='top'><a href='#' class='info'><?php echo _("Device List:")."<span><br>"._("Select Device(s) to restart.  Currently, only Aastra, Snom, Polycom, Grandstream and Cisco devices are supported.  All other devices will not show up in this list.  Click the \"Select All\" button to restart all supported devices. ") ?>
-	<br><br></span></a></td>
-	<tr>
-	<td valign="top">
+<div class="container-fluid">
+	<h1><?php echo _('Restart Phones')?></h1>
+	<?php echo $info ?>
+	<div class = "display full-border">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="fpbx-container">
+					<div class="display full-border">
+						<form name='restart' action='' method='post'>
+							<input type='hidden' name='action' value='restart'>
+							<input type='hidden' name='display' value='restart'>
+							<!--Device List-->
+							<div class="element-container">
+								<div class="row">
+									<div class="col-md-12">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="xtnlist"><?php echo _("Device List") ?></label>
+													<i class="fa fa-question-circle fpbx-help-icon" data-for="xtnlist"></i>
+												</div>
+												<div class="col-md-9">
+													<div class="input-group">
+														<select class="form-control" id="xtnlist" multiple="multiple" name="restartlist[]">
+															<?php
+															$selected = isset($selected)?$selected:array();
+															foreach ($device_list as $device) {
+																if($ua = get_device_useragent($device[0]))  {
+																	echo '<option value="'.$device[0].'" ';
+																	if (array_search($device[0], $selected) !== false) echo ' selected="selected" ';
+																	echo '>'.$device[0].' - '.$device[1].' - '.ucfirst($ua).' Device</option>';
+																}
+															}
+															?>
+														</select>
+														<span class="input-group-addon" id="deviceaddon">
+															<input type="button" name="Button" value="<?php echo _('SELECT ALL'); ?>" onclick="selectAll('xtnlist',true)" />
+														</span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<span id="xtnlist-help" class="help-block fpbx-help-block"><?php echo _("Select Device(s) to restart.  Currently, only Aastra, Snom, Polycom, Grandstream and Cisco devices are supported.  All other devices will not show up in this list.  Click the \"Select All\" button to restart all supported devices.")?></span>
+									</div>
+								</div>
+							</div>
+							<!--END Device List-->
+							<?php
+										// implementation of module hook
+										$module_hook = moduleHook::create();
+										echo $module_hook->hookHtml;
+							?>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-	<select multiple="multiple" name="restartlist[]" id="xtnlist"  tabindex="<?php echo ++$tabindex;?>">
-	<?php
-	if (is_null($selected)) $selected = array();
-	foreach (core_devices_list() as $device) {
-		if($ua = get_device_useragent($device[0]))  {
-			echo '<option value="'.$device[0].'" ';
-			if (array_search($device[0], $selected) !== false) echo ' selected="selected" ';
-			echo '>'.$device[0].' - '.$device[1].' - '.ucfirst($ua).' Device</option>';
-		}
-	}
-	?>
-	</select>
-	</td></tr>
-	<tr><td><input type="button" name="Button" value="<?php echo _('SELECT ALL'); ?>" onclick="selectAll('xtnlist',true)" /></td></tr>
-
-<?php
-			// implementation of module hook
-			$module_hook = moduleHook::create();
-			echo $module_hook->hookHtml;
-?>
-
-	<tr>
-	<td colspan="2"><br><h6><input type="submit" name="Submit" type="button" value="<?php echo _("Restart Phones")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>
-	</tr>
-	</table>
-	</form>
-	<script language="javascript">
+<script language="javascript">
 	<!-- hide script from older browsers
 
 	function selectAll(selectBox,selectAll) {
